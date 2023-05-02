559fb7c51fd0044d075f2ddfc8cc922a0e1b0299
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-05-02 10:11:36 +0300

    modified restaurant_pizza migration

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_065632) do
 
   create_table "restaurant_pizzas", force: :cascade do |t|
     t.integer "price"
+    t.integer "pizza_id"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
