192c104beb365fbbf71323d9a0f37c0f1d889a7b
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-05-02 12:01:30 +0300

    created a destroy method for restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -17,6 +17,17 @@ class RestaurantsController < ApplicationController
 
     end
 
+    def destroy
+        restaurant = Restaurant.find_by(id: params[:id])
+        if restaurant 
+            # restaurant.restaurant_pizzas.destroy_all
+            restaurant.destroy 
+            head :no_content
+        else
+            render json: { error: "Restaurant not found" }, status: :not_found 
+        end
+    end
+
     private
 
     def restaurant_json(restaurant)
